cfb9437d41a2112852093ff6b1cd2c9bbabdd034
mettroid <jorik006006@mail.ru>
2023-03-29 19:35:04 +1200

    refactor(showInstruction): удалить модуль

--- src/modules/showInstruction.mjs
+++
@@ -1,6 +0,0 @@
-export default function(id, currentActiveInstruction, classActive){
-                            let elem = document.getElementById(id);
-                            currentActiveInstruction.classList.remove(classActive);
-                            elem.classList.add(classActive);
-                            return elem;
-}
