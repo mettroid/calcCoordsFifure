2e3d5656d493093c266532e8652be061dc5d7141
mettroid <jorik006006@mail.ru>
2023-04-01 22:45:59 +1200

    feat(point.mjs): добавить условие для нахождения крайних точек прямоугольника

--- src/modules/point.mjs
+++ src/modules/point.mjs
@@ -1,7 +1,13 @@
 const draw = function(ctx, coords){ //нам нужны x и y , больше ничего
+    let x, y;
     for(let key in coords){
         if(Array.isArray(coords[key])){
-            let [x, y] = coords[key];
+            if(key === 'size'){ 
+                x = coords[key][0] + coords['point1'][0];
+                y = coords[key][1] + coords['point1'][1];
+             } else {
+                [x, y] = coords[key];
+             }       
             ctx.beginPath();
             ctx.fillStyle = 'red';
             ctx.arc(x,y,5, 0,Math.PI * 2);
