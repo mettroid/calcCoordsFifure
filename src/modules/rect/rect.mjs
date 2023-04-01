639da21cf4b720a2fd2710cb9c06c66d3e4051d7
mettroid <jorik006006@mail.ru>
2023-04-01 22:56:15 +1200

    refactor(rect.mjs): заменить значения width и height на массив size

--- src/modules/rect/rect.mjs
+++ src/modules/rect/rect.mjs
@@ -1,6 +1,6 @@
 const name = 'rect';
-export const draw = function(ctx, {point1,width,height}){
+export const draw = function(ctx, {point1,size}){
     ctx.clearRect(0,0,300,300);
     ctx.lineWidth = 2;
-    ctx.strokeRect(...point1,width,height);
+    ctx.strokeRect(...point1,...size);
 }
