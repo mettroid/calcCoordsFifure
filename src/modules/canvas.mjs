abeac24b1221355e08696cab5e760129081520ec
mettroid <jorik006006@mail.ru>
2023-04-02 22:08:18 +1200

    feat(canvas.mjs): добавить полезные св-ва отступы в возвращаемый объект

--- src/modules/canvas.mjs
+++ src/modules/canvas.mjs
@@ -6,11 +6,14 @@ const create = function(parent, id, classStyle, classPosition, width, height){
     canvas.height = height;
     canvas.id = id;
     canvas.classList.add(classStyle, classPosition);
-     
+    
+    let coords = canvas.getBoundingClientRect();
     return {
+        id,
         canvas,
         ctx: canvas.getContext('2d'),
-        id
+        offsetX: coords.x + window.pageXOffset,
+        offsetY: coords.y + window.pageYOffset
     }
 }
 export {create}
