9a32e58f579131446b64f75b2d8f52e96e3e29ff
mettroid <jorik006006@mail.ru>
2023-04-02 18:29:49 +1200

    delete(Module): удалить 2 модуля

--- src/modules/getCoords.mjs
+++
@@ -1,6 +0,0 @@
-export default (e, obj)=>{
-	let coords = {};
-	coords.x = e.clientX - obj.x;
-	coords.y = e.clientY - obj.y;
-	return coords;
-}
--- src/modules/removeActive.mjs
+++
@@ -1,7 +0,0 @@
-export const remove = function(collection, classActive){
-       collection.forEach(tag => {
-              tag.classList.contains(classActive) && tag.classList.remove(classActive);
-              
-       });
-
-}
