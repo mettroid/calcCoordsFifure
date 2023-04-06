2c99d8ccf26b1f917624105d45d918c8349fedde
mettroid <jorik006006@mail.ru>
2023-04-06 18:58:47 +1200

    feat(point): добавить вторую точку pointR2 для arc

--- src/modules/calc.mjs
+++ src/modules/calc.mjs
@@ -10,9 +10,10 @@ const rect = function(key, mouseX, mouseY, offsetX, offsetY, coords){
         }   
 }
 const arc = function(key, mouseX, mouseY, offsetX, offsetY, coords){
-        if(key === 'pointR'){
+        if(key.startsWith('pointR')){
            let a = (mouseX - offsetX) - coords.point1[0]; // катет a прямоугольного треугольника
            let b = (mouseY - offsetY) - coords.point1[1]; // катет b прямоугольного треугольника
+           console.log(a);
            let a2 = Math.pow(a, 2); 
            let b2 = Math.pow(b, 2);
            let c2 = a2 + b2; // сумма квадратов катетов = квадрат гипотенузы
@@ -21,31 +22,39 @@ const arc = function(key, mouseX, mouseY, offsetX, offsetY, coords){
            let tg = 0;
            let rad = 0;
            if(a > 0 && b > 0){
-                tg = b / a;
-                rad = 0;
+                   tg = b / a;
+                   rad = 0;
            }
            if(a <= 0 && b > 0){
-                tg = Math.abs(a) / b;
-                rad = Math.PI * 0.5;
+                   tg = Math.abs(a) / b;
+                   rad = Math.PI * 0.5;
            }
            if(a <= 0 && b <= 0){
-                tg = b / a;
-                rad = Math.PI;
+                   tg = b / a;
+                   rad = Math.PI;
            }
            if(a >= 0 && b <= 0){
                tg = a / Math.abs(b);
                rad = Math.PI * 1.5;
            }
-         
-           console.log(Math.atan(tg));
-           coords.degs[0] = Math.atan(tg) + rad;
+           if(key === 'pointR1'){
+                coords.degs[0] = Math.atan(tg) + rad;
+           } else {
+                coords.degs[1] = Math.atan(tg) + rad;
+           }
            
+           console.log(Math.atan(tg)); 
         }
         if(key === 'point1'){
-            let diffX = coords.pointR[0] - coords.point1[0];
-            let diffY = coords.pointR[1] - coords.point1[1];
-            coords.pointR[0] = mouseX - offsetX + diffX;
-            coords.pointR[1] = mouseY - offsetY + diffY;
+            console.log(coords);
+            let diffx1 = coords.pointR1[0] - coords.point1[0];
+            let diffy1 = coords.pointR1[1] - coords.point1[1];
+            let diffx2 = coords.pointR2[0] - coords.point1[0];
+            let diffy2 = coords.pointR2[1] - coords.point1[1];
+            coords.pointR1[0] = mouseX - offsetX + diffx1;
+            coords.pointR1[1] = mouseY - offsetY + diffy1;
+            coords.pointR2[0] = mouseX - offsetX + diffx2;
+            coords.pointR2[1] = mouseY - offsetY + diffy2;
         }
 }
 export {rect, arc}
--- src/modules/point.mjs
+++ src/modules/point.mjs
@@ -33,10 +33,12 @@ function addPoints(coordsFigure, figure){ //обычный объект с ра�
              coordsFigure.point2 = [x, y];
         break;
         case ('arc'):
-            if('pointR' in coordsFigure) return;
+            if('pointR1' in coordsFigure && 
+               'pointR2' in coordsFigure) return;
             x = coordsFigure.radius + coordsFigure.point1[0]; //радиус + x
             y = coordsFigure.point1[1];  // y
-            coordsFigure.pointR = [x, y]; 
+            coordsFigure.pointR1 = [x, y];
+            coordsFigure.pointR2 = [x, y] 
         break;
         case ('arcTo'):
         break;
