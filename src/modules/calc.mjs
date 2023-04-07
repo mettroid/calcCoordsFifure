7550f841f6016d281af761f936dd5c51a34018e2
mettroid <jorik006006@mail.ru>
2023-04-07 23:58:31 +1200

    feat(func): добавить вспомогательные функции setRadius setDeg

--- src/modules/calc.mjs
+++ src/modules/calc.mjs
@@ -1,4 +1,9 @@
-
+let sinA = null;
+let cosA = null;
+const clear = function(){
+        sinA = null;
+        cosA = null;
+}
 const rect = function(key, mouseX, mouseY, offsetX, offsetY, coords){
          if(key === 'point2'){
             coords.size[0] = mouseX - offsetX - coords.point1[0]; //изменили ширину квадрата
@@ -11,39 +16,33 @@ const rect = function(key, mouseX, mouseY, offsetX, offsetY, coords){
 }
 const arc = function(key, mouseX, mouseY, offsetX, offsetY, coords){
         if(key.startsWith('pointR')){
-           let a = (mouseX - offsetX) - coords.point1[0]; // катет a прямоугольного треугольника
-           let b = (mouseY - offsetY) - coords.point1[1]; // катет b прямоугольного треугольника
-           console.log(a);
-           let a2 = Math.pow(a, 2); 
-           let b2 = Math.pow(b, 2);
-           let c2 = a2 + b2; // сумма квадратов катетов = квадрат гипотенузы
-           coords.radius = Math.sqrt(c2);
+                let sides = setRadius(key, mouseX, mouseY, offsetX, offsetY, coords);
+                
+                sides.then(({a,b,c})=>{
+                        console.log(b);
+                        setDeg(a, b, coords, key);
+                        if(key === 'pointR1'){
+                                if(sinA === null && cosA === null){
+                                        let a = coords.pointR2[0] - coords.point1[0];
+                                        let b = coords.pointR2[1] - coords.point1[1];  
+                                        let c2 = Math.pow(b, 2) + Math.pow(a, 2);
+                                        let c = Math.sqrt(c2);
+                                        sinA = b / c;
+                                        cosA = a / c; 
+                                        console.log(sinA); 
+                                }
+                                let height = coords.radius * sinA;
+                                let width = coords.radius * cosA;
+                                coords.pointR2[0] = coords.point1[0] + width;
+                                coords.pointR2[1] = coords.point1[1] + height;
+                
+                                //console.log(width + ' ' + height);
+                                console.log(height);
+                
+                           } else {
+                           }
+                });
 
-           let tg = 0;
-           let rad = 0;
-           if(a > 0 && b > 0){
-                   tg = b / a;
-                   rad = 0;
-           }
-           if(a <= 0 && b > 0){
-                   tg = Math.abs(a) / b;
-                   rad = Math.PI * 0.5;
-           }
-           if(a <= 0 && b <= 0){
-                   tg = b / a;
-                   rad = Math.PI;
-           }
-           if(a >= 0 && b <= 0){
-               tg = a / Math.abs(b);
-               rad = Math.PI * 1.5;
-           }
-           if(key === 'pointR1'){
-                coords.degs[0] = Math.atan(tg) + rad;
-           } else {
-                coords.degs[1] = Math.atan(tg) + rad;
-           }
-           
-           console.log(Math.atan(tg)); 
         }
         if(key === 'point1'){
             console.log(coords);
@@ -57,4 +56,40 @@ const arc = function(key, mouseX, mouseY, offsetX, offsetY, coords){
             coords.pointR2[1] = mouseY - offsetY + diffy2;
         }
 }
-export {rect, arc}
+const setDeg = function(a, b, coords, key){
+        let tg = 0;
+        let rad = 0;
+        if(a > 0 && b > 0){
+                tg = b / a;
+                rad = 0;
+        } 
+        if(a <= 0 && b > 0){
+                tg = Math.abs(a) / b;
+                rad = Math.PI * 0.5;
+        }
+        if(a <= 0 && b <= 0){
+                tg = b / a;
+                rad = Math.PI;
+        }
+        if(a >= 0 && b <= 0){
+            tg = a / Math.abs(b);
+            rad = Math.PI * 1.5;
+        }
+        if(key === 'pointR1'){
+                coords.degs[0] = Math.atan(tg) + rad;
+        } else {
+                coords.degs[1] = Math.atan(tg) + rad;
+        } 
+}
+const setRadius = function(key, mouseX, mouseY, offsetX, offsetY, coords){
+        return new Promise(function(resolve, reject){
+                (async()=>{
+                        let Math = await import('./math.mjs');
+                        let sides = Math.getSides(mouseX, mouseY, offsetX, offsetY, coords);
+                        coords.radius = sides.c;
+                        resolve(sides);
+                })();
+        });
+
+}
+export {rect, arc, clear}
