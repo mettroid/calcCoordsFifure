046d5bad2d200303ed3532540d4a438238c853a7
mettroid <jorik006006@mail.ru>
2023-04-08 22:40:35 +1200

    feat(coordsLine.mjs): добавить метод toString

--- src/modules/line/coordsLine.mjs
+++ src/modules/line/coordsLine.mjs
@@ -1,4 +1,8 @@
 export const coords = {
     point1: [100, 100],
-    point2: [200, 200]
+    point2: [200, 200],
+    toString(){
+        return `ctx.moveTo(${this.point1[0]}, ${this.point1[1]});
+                ctx.lineTo(${this.point2[0]}, ${this.point2[1]});`;
+    }
 }
